6014192ca0ca597cc1a29e892393c33128c325f4
unknown <viswasvarmasatya@msitprogram.net>
2019-03-14 15:25:01 +0530

    Activity 19

---
+++ M2/A16/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.recyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private  static int NUM_LIST_ITEMS = 101;
+
+
+    private GreenAdapter mAdapter;
+    private RecyclerView mNumberList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mNumberList = (RecyclerView)findViewById(R.id.rv_numbers);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+            mNumberList.setLayoutManager(layoutManager);
+            mNumberList.setHasFixedSize(true);
+            mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+            mNumberList.setAdapter(mAdapter);
+    }
+}
